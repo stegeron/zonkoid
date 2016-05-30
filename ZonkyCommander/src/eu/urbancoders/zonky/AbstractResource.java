726d76606b98cd57abd5d9f3276b1e0e1cdc0847
Ondrej Steger <ondrej@steger.cz>
2016-05-31 00:21:51 +0200

    ZonkyCommander

--- ZonkyCommander/src/eu/urbancoders/zonky/AbstractResource.java
+++ ZonkyCommander/src/eu/urbancoders/zonky/AbstractResource.java
@@ -1,10 +1,10 @@
 package eu.urbancoders.zonky;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import cz.steger.urbancaching.Constants;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+//import com.google.gson.Gson;
+//import com.google.gson.GsonBuilder;
+//import cz.steger.urbancaching.Constants;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
 
 /**
  * Spolecny predek pro vsechny resources
@@ -14,21 +14,21 @@ import org.slf4j.LoggerFactory;
  */
 public class AbstractResource {
 
-    public static final Logger log = LoggerFactory.getLogger(AbstractResource.class);
-
-    private static GsonBuilder jsonBuilder = new GsonBuilder();
-
-    static {
-        jsonBuilder.setPrettyPrinting();
-        jsonBuilder.setDateFormat(Constants.Formats.DATE_FORMAT_ISO8601);
-        jsonBuilder.disableHtmlEscaping();
-    }
-
-    public String toJson(Object o) {
-        synchronized (jsonBuilder) {
-            Gson gson = jsonBuilder.create();
-            return gson.toJson(o);
-        }
-    }
+//    public static final Logger log = LoggerFactory.getLogger(AbstractResource.class);
+//
+//    private static GsonBuilder jsonBuilder = new GsonBuilder();
+//
+//    static {
+//        jsonBuilder.setPrettyPrinting();
+//        jsonBuilder.setDateFormat(Constants.Formats.DATE_FORMAT_ISO8601);
+//        jsonBuilder.disableHtmlEscaping();
+//    }
+//
+//    public String toJson(Object o) {
+//        synchronized (jsonBuilder) {
+//            Gson gson = jsonBuilder.create();
+//            return gson.toJson(o);
+//        }
+//    }
 
 }
--- ZonkyCommander/src/eu/urbancoders/zonky/ZonkyCommanderApplication.java
+++ ZonkyCommander/src/eu/urbancoders/zonky/ZonkyCommanderApplication.java
@@ -1,11 +1,11 @@
 package eu.urbancoders.zonky;
 
-import cz.steger.urbancaching.server.filters.CharsetResponseFilter;
-import org.glassfish.jersey.jackson.JacksonFeature;
-import org.glassfish.jersey.media.multipart.MultiPartFeature;
-import org.glassfish.jersey.server.ResourceConfig;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+//import cz.steger.urbancaching.server.filters.CharsetResponseFilter;
+//import org.glassfish.jersey.jackson.JacksonFeature;
+//import org.glassfish.jersey.media.multipart.MultiPartFeature;
+//import org.glassfish.jersey.server.ResourceConfig;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
 
 /**
  * Starter JAX-RS aplikace, nastaveni resourcu, singleton cachi apod.
@@ -13,19 +13,19 @@ import org.slf4j.LoggerFactory;
  * Author: Ondrej Steger (ondrej@steger.cz)
  * Date: 12/25/14
  */
-public class ZonkyCommanderApplication extends ResourceConfig {
+public class ZonkyCommanderApplication {//extends ResourceConfig {
 
-    private static final Logger log = LoggerFactory.getLogger(UrbanCachingApplication.class);
-
-    public ZonkyCommanderApplication() {
-        super(
-                AdministrationResource.class,
-                PoisResource.class,
-                JacksonFeature.class,
-                POJOMapperProvider.class,
-                CharsetResponseFilter.class,
-                MultiPartFeature.class
-
-        );
-    }
+//    private static final Logger log = LoggerFactory.getLogger(UrbanCachingApplication.class);
+//
+//    public ZonkyCommanderApplication() {
+//        super(
+//                AdministrationResource.class,
+//                PoisResource.class,
+//                JacksonFeature.class,
+//                POJOMapperProvider.class,
+//                CharsetResponseFilter.class,
+//                MultiPartFeature.class
+//
+//        );
+//    }
 }
