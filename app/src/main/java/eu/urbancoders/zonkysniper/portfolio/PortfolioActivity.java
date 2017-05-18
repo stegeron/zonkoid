d964e21a7e7ab99ab287de24951332d3b49ea11e
Ondrej Steger <ondrej@steger.cz>
2017-05-19 00:23:50 +0200

    uprava grafu a facelift penezenky

--- app/src/main/java/eu/urbancoders/zonkysniper/portfolio/PortfolioActivity.java
+++ app/src/main/java/eu/urbancoders/zonkysniper/portfolio/PortfolioActivity.java
@@ -110,7 +110,7 @@ public class PortfolioActivity extends ZSViewActivity {
         @Override
         public int getCount() {
             // Show 3 total pages.
-            return 3;
+            return 2;
         }
 
         @Override
